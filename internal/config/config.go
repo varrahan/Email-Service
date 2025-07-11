c7d1862c38c7bafe4155933d16f5a599c251a4e5
varrahan <varrahan@gmail.com>
2025-07-11 17:20:18 -0400

    Add config

---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+    SMTPHost     string
+    SMTPPort     int
+    SMTPUser     string
+    SMTPPass     string
+    FromAddr	 string
+}
+
+func GetConfig() *Config {
+    portStr := os.Getenv("PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		fmt.Println("Error converting port:", err)
+		port = 8000
+	}
+
+    return &Config{
+		SMTPHost: os.Getenv("SMTP_HOST"),
+        SMTPPort: port,
+        SMTPUser: os.Getenv("SMTP_USER"),
+        SMTPPass: os.Getenv("SMTP_PASS"),
+        FromAddr: os.Getenv("FROM_ADDR"),
+    }
+}
