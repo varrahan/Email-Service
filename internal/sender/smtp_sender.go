b077a31f293980369bb629f52c150d9d571611d5
Varrahan <varrahan@gmail.com>
2026-01-08 14:50:24 -0500

    Add logging

--- internal/sender/smtp_sender.go
+++ internal/sender/smtp_sender.go
@@ -5,6 +5,7 @@ import (
 	"email-service/internal/model"
 
 	"github.com/wneessen/go-mail"
+	"go.uber.org/zap"
 )
 
 type Sender interface {
@@ -18,9 +19,10 @@ type SmtpSender struct {
 	pass     string
 	toAddr   string
 	fromAddr string
+	logger   *zap.Logger
 }
 
-func NewSmtpSender(host string, port int, user, pass, to string, from string) *SmtpSender {
+func NewSmtpSender(host string, port int, user, pass, to string, from string, logger *zap.Logger) *SmtpSender {
 	return &SmtpSender{
 		host:     host,
 		port:     port,
@@ -28,20 +30,39 @@ func NewSmtpSender(host string, port int, user, pass, to string, from string) *S
 		pass:     pass,
 		toAddr:	  to,
 		fromAddr: from,
+		logger:   logger,
 	}
 }
 
 func (s *SmtpSender) Send(ctx context.Context, email model.Email) error {
 	m := mail.NewMsg()
 	if err := m.From(s.fromAddr); err != nil {
+		s.logger.Error(
+			"Error with setting FROM address", 
+			zap.String("sender", email.Email),
+			zap.Error(err),
+		)
 		return err
 	}
 	if err := m.To(s.toAddr); err != nil { 
+		s.logger.Error(
+			"Error with setting TO address", 
+			zap.String("sender", email.Email),
+			zap.Error(err),
+		)
 		return err
 	}
 	m.Subject(email.Subject)
 	m.SetBodyString(mail.TypeTextPlain, email.Message)
-	m.ReplyTo(email.Email) // email of person who submitted the form
+
+	if err := m.ReplyTo(email.Email); err != nil {
+		s.logger.Error(
+			"Error with setting ReplyTo address", 
+			zap.String("sender", email.Email),
+			zap.Error(err),
+		)
+		return err
+	}
 
 	client, err := mail.NewClient(
 		s.host,
@@ -53,6 +74,11 @@ func (s *SmtpSender) Send(ctx context.Context, email model.Email) error {
 		
 	)
 	if err != nil {
+		s.logger.Error(
+			"Error with creating client instance", 
+			zap.String("sender", email.Email),
+			zap.Error(err),
+		)
 		return err
 	}
 	return client.DialAndSend(m)
