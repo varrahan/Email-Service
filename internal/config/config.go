9f4fd83c6449ab37f59482ca911c51865b18ad57
Varrahan <varrahan@gmail.com>
2026-01-08 16:11:42 -0500

    Change from GetEnv to LoadEnv, and support App port

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,22 +3,54 @@ package config
 import (
 	"os"
 	"strconv"
+
+    "github.com/joho/godotenv"
 )
 
 type Config struct {
     SMTPHost    string
-    SMTPPort    int
+    SMTPPort    int    // Port provided by third-party SMTP service
     SMTPUser    string
     SMTPPass    string
     ToAddr      string // Personal or service email
     FromAddr    string // Email connected to SMTP provider
+    AppPort     int    // Port running the application
+}
+
+func LoadConfig() *Config {
+    godotenv.Load()
+
+    smtpPortStr := os.Getenv("SMTP_PORT")
+	smtpPort, err := strconv.Atoi(smtpPortStr)
+	if err != nil {
+		smtpPort = 587
+	}
+
+    appPortStr := os.Getenv("APP_PORT")
+	appPort, err := strconv.Atoi(appPortStr)
+	if err != nil {
+		appPort = 8080
+	}
+
+    return &Config{
+		SMTPHost: os.Getenv("SMTP_HOST"),
+        SMTPPort: smtpPort,
+        SMTPUser: os.Getenv("SMTP_USER"),
+        SMTPPass: os.Getenv("SMTP_PASS"),
+        ToAddr: os.Getenv("TO_ADDRESS"),
+        FromAddr: os.Getenv("FROM_ADDRESS"),
+        AppPort: appPort,
+    } 
 }
 
 func GetConfig() *Config {
-    portStr := os.Getenv("PORT")
+
+    godotenv.Load()
+
+    portStr := os.Getenv("SMTP_PORT")
 	port, err := strconv.Atoi(portStr)
 	if err != nil {
-		port = 8000
+		port = 587
 	}
 
     return &Config{
