89613ed801757dfe69a6564a68202513bf03a598
Varrahan <varrahan@gmail.com>
2026-01-08 14:50:20 -0500

    Add logging

--- internal/handler/email_handler.go
+++ internal/handler/email_handler.go
@@ -5,23 +5,39 @@ import (
 	"github.com/gin-gonic/gin"
 	"email-service/internal/model"
 	"email-service/internal/service"
+
+	"go.uber.org/zap"
 )
 
-type Handler struct {
-	emailService *service.EmailService
+type EmailHandler struct {
+	service *service.EmailService
+	logger *zap.Logger
 }
 
-func NewHandler(emailService *service.EmailService) *Handler {
-	return &Handler{emailService: emailService}
+func NewEmailHandler(emailService *service.EmailService, logger *zap.Logger) *EmailHandler {
+	return &EmailHandler{
+		service: emailService,
+		logger: logger,
+	}
 }
 
-func (h *Handler) SendEmail(c *gin.Context) {
+func (h *EmailHandler) SendEmail(c *gin.Context) {
 	var req model.Email
+	h.logger.Debug("handling send email request")
+
 	if err := c.ShouldBindJSON(&req); err != nil {
+		h.logger.Error(
+			"Error with binding request data to variable", 
+			zap.Error(err),
+		)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	if err := h.emailService.SendContactEmail(c.Request.Context(), req); err != nil {
+	if err := h.service.SendContactEmail(c.Request.Context(), req); err != nil {
+		h.logger.Error(
+			"Error with sending email", 
+			zap.Error(err),
+		)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to send email:",})
 		return
 	}
