8707554430e2ddd27fef8d56aea5951cca5e8a65
Varrahan <varrahan@gmail.com>
2026-01-08 00:43:47 -0500

    Add logging

--- internal/service/email_service.go
+++ internal/service/email_service.go
@@ -4,17 +4,35 @@ import (
 	"context"
 	"email-service/internal/model"
 	"email-service/internal/sender"
+
+	"go.uber.org/zap"
 )
 
 type EmailService struct {
 	sender sender.Sender
+	logger *zap.Logger
 }
 
-func NewEmailService(s sender.Sender) *EmailService {
-	return &EmailService{sender: s}
+func NewEmailService(sender sender.Sender, logger *zap.Logger) *EmailService {
+	return &EmailService{
+		sender: sender,
+		logger: logger,
+	}
 }
 
 func (s *EmailService) SendContactEmail(ctx context.Context, email model.Email) error {
-	// Here you could validate, log, etc.
-	return s.sender.Send(ctx, email)
+	s.logger.Info("receiving and sending email",
+		zap.String("sender", email.Email),
+		zap.String("subject", email.Subject),
+	)
+
+	err := s.sender.Send(ctx,email)
+	if err != nil {
+		s.logger.Error("Failed to send email",
+			zap.String("sender", email.Email),
+			zap.Error(err),
+		)
+	}
+
+	return nil
 }
