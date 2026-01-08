b68cca3a766157c20e25a7d0413094169164fb05
Varrahan <varrahan@gmail.com>
2026-01-07 23:59:00 -0500

    Update sender

--- internal/sender/smtp_sender.go
+++ internal/sender/smtp_sender.go
@@ -16,36 +16,41 @@ type SmtpSender struct {
 	port     int
 	user     string
 	pass     string
+	toAddr   string
 	fromAddr string
 }
 
-func NewSmtpSender(host string, port int, user, pass, from string) *SmtpSender {
+func NewSmtpSender(host string, port int, user, pass, to string, from string) *SmtpSender {
 	return &SmtpSender{
 		host:     host,
 		port:     port,
 		user:     user,
 		pass:     pass,
+		toAddr:	  to,
 		fromAddr: from,
 	}
 }
 
 func (s *SmtpSender) Send(ctx context.Context, email model.Email) error {
 	m := mail.NewMsg()
-	if err := m.From(s.fromAddr); err != nil {
+	if err := m.From(s.user); err != nil {
 		return err
 	}
-	if err := m.To(s.fromAddr); err != nil { // receiving inbox
+	if err := m.To(s.toAddr); err != nil { 
 		return err
 	}
 	m.Subject(email.Subject)
 	m.SetBodyString(mail.TypeTextPlain, email.Message)
-	m.ReplyTo(email.Email) // visitor's email so you can reply directly
+	m.ReplyTo(email.Email) // email of person who submitted the form
 
 	client, err := mail.NewClient(
 		s.host,
 		mail.WithPort(s.port),
 		mail.WithSMTPAuth(mail.SMTPAuthPlain),
 		mail.WithTLSPolicy(mail.TLSMandatory),
+		mail.WithUsername(s.user),
+		mail.WithPassword(s.pass),
+		
 	)
 	if err != nil {
 		return err
