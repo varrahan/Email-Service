a2ac94e0fc6fcff8f45f92cb1ef79548065a65fb
Varrahan <varrahan@gmail.com>
2026-01-09 02:52:33 -0500

    Add nginx entrypoint script

---
+++ nginx-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+: "${APP_PORT:?APP_PORT not set}"
+: "${NGINX_PORT:?NGINX_PORT not set}"
+
+# Replace env variables in nginx template
+envsubst '$NGINX_PORT $APP_PORT' < /etc/nginx/templates/nginx.template.conf > /etc/nginx/conf.d/default.conf
+
+# Start NGINX in foreground
+nginx -g 'daemon off;'
