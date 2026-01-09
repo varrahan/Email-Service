c788c31aee25f82d86a891aa7a7c59e0e120178d
Varrahan <varrahan@gmail.com>
2026-01-09 02:52:52 -0500

    Add golang service entrypoint

---
+++ service-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+: "${APP_PORT:?APP_PORT not set}"
+: "${SMTP_HOST:?SMTP_HOST not set}"
+: "${SMTP_PORT:?SMTP_PORT not set}"
+: "${SMTP_USER:?SMTP_USER not set}"
+: "${SMTP_PASS:?SMTP_PASS not set}"
+: "${TO_ADDRESS:?TO_ADDRESS not set}"
+: "${FROM_ADDRESS:?FROM_ADDRESS not set}"
+
+./email-service
