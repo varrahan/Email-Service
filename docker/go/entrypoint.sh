f30a34ec2e56b09657296845f6ad2e0f91b99619
Varrahan <varrahan@gmail.com>
2026-01-09 03:09:41 -0500

    Add script to new folder

---
+++ docker/go/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+# Check if env variables are loaded in container
+: "${APP_PORT:?APP_PORT not set}"
+: "${SMTP_HOST:?SMTP_HOST not set}"
+: "${SMTP_PORT:?SMTP_PORT not set}"
+: "${SMTP_USER:?SMTP_USER not set}"
+: "${SMTP_PASS:?SMTP_PASS not set}"
+: "${TO_ADDRESS:?TO_ADDRESS not set}"
+: "${FROM_ADDRESS:?FROM_ADDRESS not set}"
+
+# Run application
+./email-service
