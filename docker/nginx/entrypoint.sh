207714bf87e055b3ab03e883e25442da5ea3fc3d
Varrahan <varrahan@gmail.com>
2026-01-09 23:58:49 -0500

    Update entrypoint script

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Check if env variables are loaded in container
-: "${APP_PORT:?APP_PORT not set}"
+: "${PORT:?PORT not set}"
 : "${NGINX_PORT:?NGINX_PORT not set}"
 
 # Replace env variables in nginx template
-envsubst '$NGINX_PORT $APP_PORT' < /etc/nginx/templates/nginx.template.conf > /etc/nginx/conf.d/default.conf
+envsubst '$NGINX_PORT $PORT' < /etc/nginx/templates/nginx.template.conf > /etc/nginx/conf.d/default.conf
 
 # Start NGINX in foreground
 nginx -g 'daemon off;'
